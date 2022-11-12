236a5f794640a02fdc4cea5efde3c5e5925a43a3
狐日泽 <shdocter@nya-wsl.com>
2022-11-12 22:48:00 +0800

    修改下一步快捷键，防止误触

--- static/js/src/playground/player/zbase.js
+++ static/js/src/playground/player/zbase.js
@@ -98,7 +98,7 @@ export class Player extends AcGameObject {
             } else if (e.which === 38) {  // ↑，放炸弹
                 outer.use_bomb();
                 return false;
-            } else if (e.which === 13) {  // Enter，点击下一步按钮
+            } else if (e.ctrlKey && e.which === 13) {  // Enter，点击下一步按钮
                 if (outer.playground.character === "shop") {  // 在商店界面按Enter会进入下一关
                     outer.shop.use_item_control_player_behavior_in_shop(5);
                 } else if (outer.playground.character === "pop up") {  // 在弹窗界面按Enter会开始游戏或进入商店
